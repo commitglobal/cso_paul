17accbb0036fe670db4732159720d9fa8b44ef73
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-06-16 14:58:30 +0300

    Initial commit: Start work on Paul 2.0

---
+++ backend/hello/views.py
@@ -0,0 +1,37 @@
+from typing import Any, Dict
+
+from django.conf import settings
+from django.http import HttpResponse, HttpRequest
+from django.utils import timezone
+from django.views.decorators.cache import cache_control, never_cache
+from inertia import inertia
+
+
+@cache_control(private=False)
+@inertia("Public/Home/Index")
+def home(request: HttpRequest) -> Dict[str, Any]:
+    """
+    Public homepage data
+    """
+    return {"ok": True}
+
+
+@never_cache
+def health(request: HttpRequest) -> HttpResponse:
+    """
+    Health check endpoint
+    """
+    normal_response_text = f"OK - {timezone.now()}"
+
+    # Show detailed information only to authenticated staff members
+    if request.user.is_authenticated and (
+        request.user.is_staff
+        or request.user.is_superuser
+        or request.user.is_admin_member
+        or request.user.is_superadmin_member
+    ):
+        return HttpResponse(
+            f"{normal_response_text} [user #{request.user.pk}] [{settings.VERSION} {settings.REVISION}]"
+        )
+
+    return HttpResponse(normal_response_text)
---
+++ backend/paul/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for paul project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+
+from dashboard.admin import dashboard_site
+
+
+urlpatterns = [
+    # TODO: Remove the next couple of rules because they were intended for an Unfold based site
+    path("admin/", admin.site.urls),  # these URLs use the "admin:" prefix
+    path("app/", dashboard_site.urls),  # these URLs use the "app:" prefix
+    #
+    path("", include("hello.urls")),
+]
