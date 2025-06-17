98358fdbeb18a24b669eb67ae33b0550f7729038
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2025-06-17 22:13:57 +0300

    Refactor the login screen backend

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-
+    path("login/", views.login_choice, name="login"),
+    path("login/email/", views.email_login, name="login-by-email"),
+    path("login/ngohub/", views.ngohub_login, name="login-by-ngohub"),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,1 +1,46 @@
-# Create your views here.
+from typing import Any, Dict
+
+from django.conf import settings
+from django.http import HttpResponse, HttpRequest
+from django.urls import reverse
+from django.utils import timezone
+from django.views.decorators.cache import cache_control, never_cache
+from inertia import inertia
+
+
+@cache_control(private=False)
+@inertia("Users/Login/Choice")
+def login_choice(request: HttpRequest) -> Dict[str, Any]:
+    """
+    Screen for choosing the preferred login method
+    """
+
+    return {
+        "endpoints": {
+            "ngohub": True,
+            "ngohub_url": reverse("users:login-by-ngohub"),
+            "email": True,
+            "email_url": reverse("users:login-by-email"),
+        }
+    }
+
+
+@cache_control(private=True)
+@inertia("Users/Login/Email")
+def email_login(request: HttpRequest) -> Dict[str, Any]:
+    """
+    Login by using the email and password
+    """
+
+    return {"ok": True}
+
+
+@cache_control(private=True)
+@inertia("Users/Login/Ngohub")
+def ngohub_login(request: HttpRequest) -> Dict[str, Any]:
+    """
+    Login by using NGO Hub
+    """
+
+    raise NotImplementedError("NGO Hub authentication is not implemented yet")
+
