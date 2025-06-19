270ee06be01fe8feb75606ff8288e8231d881bd3
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2025-06-19 22:49:20 +0300

    Improve the login page layout

--- frontend/src/pages/Users/Login/Email.tsx
+++ frontend/src/pages/Users/Login/Email.tsx
@@ -4,10 +4,8 @@ import LoginLayout from '@/layouts/LoginLayout';
 
 export default function Index() {
   return (
-    <div className='flex flex-col gap-y-8 items-center'>
-      <h1 className='font-bold text-xl text-black mb-6'>
-        Login to PAUL
-      </h1>
+    <div className='flex flex-col gap-y-8 m-10'>
+      <div className='font-bold text-[32px] text-black'>Login to PAUL</div>
       
       <LoginForm />
     </div>
