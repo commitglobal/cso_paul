6470a94997f5830cda170c2c701e6961cb07da5c
Tudor <tudor.amariei@commitglobal.org>
2025-06-19 09:54:37 +0300

    Switch from PostCSS to Tailwind's standalone integration (#47)
    * Switch from PostCSS to Tailwind's standalone integration
    
    - Remove `postcss.config.js`
    - Replace `postcss` dependency with `@tailwindcss/vite` and related packages in `package-lock.json`
    - Set the front-end to type module
    
    ---------
    
    Co-authored-by: Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -10,6 +11,7 @@ export default defineConfig(({ command, mode }) => {
       react({
         include: '**/*.disabled',
       }),
+      tailwindcss(),
     ],
     root: resolve(__dirname, './src'),
     base: command === 'serve' ? '/static/' : '',
