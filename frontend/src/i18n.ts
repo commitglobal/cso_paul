dcc894f5cac4d97430f1ec4340a0c6bdaccff64d
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-06-25 13:47:10 +0300

    remove en as default

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,18 +1,25 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import enJSON from './locales/en.json'
 import roJSON from './locales/ro.json'
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: enJSON },
-    ro: { translation: roJSON }
-  },
-  lng: 'en',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false
-  }
-})
+i18n
+  .use(LanguageDetector)  // Add this line
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enJSON },
+      ro: { translation: roJSON }
+    },
+    fallbackLng: 'en',
+    detection: {
+      order: ['navigator', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage']
+    },
+    interpolation: {
+      escapeValue: false
+    }
+  })
 
-export default i18n;
+export default i18n
