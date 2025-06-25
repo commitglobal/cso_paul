b8240452aee7dbbdb16d4792c10bf6ce66704244
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-06-25 13:43:58 +0300

    Add language code context processor for dynamic template language support
    - Create `env_vars.py` context processor to provide `language_code`
    - Update settings to include the new context processor
    - Modify `base.html` to dynamically set the `lang` attribute with the provided `language_code`

--- backend/paul/settings.py
+++ backend/paul/settings.py
@@ -298,6 +298,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "paul.context_processors.env_vars.main",
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
