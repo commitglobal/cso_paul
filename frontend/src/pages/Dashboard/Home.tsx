5d83a0e255a2a1ac2889f91fe202a4c5bf2ffc5e
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2025-06-30 23:22:47 +0300

    Improve the dashboard home layout

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -1,3 +1,7 @@
+import { Disclosure, DisclosureButton, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
 import { useValidatedProps } from '@/hooks/useValidatedProps';
 import { DashboardHomeProps } from './DashboardHomeProps';
 import { AppSidebar } from "@/components/app-sidebar"
@@ -23,21 +27,102 @@ export default function DashboardHome() {
 
   return (
     <>
-    <div className='fixed top-0 z-40 left-0 right-0'>
-      <div className='flex justify-between h-16 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-md sm:gap-x-6 sm:px-6 xl:shadow-none'>
-        <div className='flex gap-6 w-full'>
-          PAUL
-        </div>
-    
-        <div className='flex gap-x-4 self-stretch xl:gap-x-6'>
-          <div className='flex items-center gap-x-4 xl:gap-x-6'>
-            {/* Profile dropdown */}
 
+    <Disclosure as="nav" className="bg-white shadow">
+      <div className="mx-auto px-2 sm:px-4 lg:px-8">
+        <div className="flex h-16 justify-between ">
+          <div className="flex px-2 lg:px-0">
+            <div className="flex shrink-0 items-center">
+              <img
+                alt="Your Company"
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </div>
+
+          </div>
+          <div className="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end">
+            <div className="grid w-full max-w-lg grid-cols-1 lg:max-w-xs">
+              <input
+                name="search"
+                type="search"
+                placeholder="Search"
+                className="col-start-1 row-start-1 block w-full rounded-md bg-white py-1.5 pl-10 pr-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+              <MagnifyingGlassIcon
+                aria-hidden="true"
+                className="pointer-events-none col-start-1 row-start-1 ml-3 size-5 self-center text-gray-400"
+              />
+            </div>
+          </div>
+          <div className="flex items-center lg:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-[open]:block" />
+            </DisclosureButton>
+          </div>
+          <div className="hidden lg:ml-4 lg:flex lg:items-center">
+            <button
+              type="button"
+              className="relative shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View notifications</span>
+              <BellIcon aria-hidden="true" className="size-6" />
+            </button>
+
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative ml-4 shrink-0">
+              <div>
+                <MenuButton className="relative flex rounded-full bg-white text-sm focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2">
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    alt=""
+                    src=""
+                    className="size-8 rounded-full"
+                  />
+                </MenuButton>
+              </div>
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              >
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                  >
+                    Your Profile
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                  >
+                    Settings
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                  >
+                    Sign out
+                  </a>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
           </div>
         </div>
       </div>
-    </div>
-    <SidebarProvider className="mt-16">
+    </Disclosure>
+
+    <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
@@ -63,7 +148,12 @@ export default function DashboardHome() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          test
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
         </div>
       </SidebarInset>
     </SidebarProvider>
