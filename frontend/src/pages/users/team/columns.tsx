6798c5bc14347ad57f7f38162421bc6b18b4f5be
Ion Dormenco <idormenco@gmail.com>
2025-07-21 10:59:39 +0300

    rework table columns

--- frontend/src/pages/users/team/columns.tsx
+++ frontend/src/pages/users/team/columns.tsx
@@ -1,33 +1,35 @@
-"use client"
+"use client";
 
-import { type ColumnDef } from "@tanstack/react-table"
-
-export type UserProps = {
-  id: string
-  email: string
-  first_name: string
-  last_name: string
-  last_login: string // ISO date string
-  date_joined: string // ISO date string
-}
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import { type ColumnDef } from "@tanstack/react-table";
 
 export type User = {
-  id: string
-  user: {
-    name: string
-    email: string
-    is_current_user: boolean
-  }
-  role: "Admin" | "Manager" | "User"
-  added_since: string // ISO date string
-  last_activity: string // ISO date string
-}
+  id: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  last_login: string; // ISO date string
+  date_joined: string; // ISO date string
+};
 
-
-export const columns: ColumnDef<User>[] = [
+export const columns: (dependinta: any) => ColumnDef<User>[] = (dependinta) => [
   {
-    accessorKey: "user",
+    accessorKey: "email",
     header: "User",
+    cell: ({ row }) => (
+      <div>
+        <span>
+          {row.original.first_name} {row.original.last_name} {"(you)"}
+        </span>
+        <span>{row.original.email}</span>
+      </div>
+    ),
   },
   {
     accessorKey: "role",
@@ -41,4 +43,22 @@ export const columns: ColumnDef<User>[] = [
     accessorKey: "last_activity",
     header: "Last activity",
   },
-]
+
+  {
+    accessorKey: "actions",
+    cell: ({ row }) => (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <EllipsisVerticalIcon className="w-[24px] h-[24px] text-purple-600" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => console.log("view")}>View {row.original.id}</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => console.log("edit")}>Edit</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => console.log("resend")}>
+            Resend invitation email {dependinta}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    ),
+  },
+];
--- frontend/src/pages/users/team/index.tsx
+++ frontend/src/pages/users/team/index.tsx
@@ -1,29 +1,32 @@
-import { columns, type User, type UserProps } from "./columns";
-import { useValidatedProps } from "@/hooks/use-validated-props.ts";
-import { LoginChoiceProps } from "@/pages/users/auth/login-choice-props.ts";
-import BaseLayout from "@/layouts/base-layout.tsx";
 import { DataTable } from "@/components/ui/data-table";
+import { useValidatedProps } from "@/hooks/use-validated-props.ts";
+import BaseLayout from "@/layouts/base-layout.tsx";
+import { LoginChoiceProps } from "@/pages/users/auth/login-choice-props.ts";
+import { useMemo } from "react";
+import { columns, type User } from "./columns";
 
-function getData(users: UserProps[]): User[] {
-  return users.map((user) => ({
-    id: user.id,
-    user: {
-      name: `${user.first_name} ${user.last_name}`,
-      email: user.email,
-      is_current_user: false, // This can be set based on your logic
-    },
-    role: "User", // Default role, you can modify this based on your logic
-    added_since: new Date(user.date_joined).toLocaleDateString(),
-    last_activity: new Date(user.last_login).toLocaleDateString(),
-  }));
-}
+// function getData(users: User[]): UserProps[] {
+//   return users.map((user) => ({
+//     id: user.id,
+//     user: {
+//       name: `${user.first_name} ${user.last_name}`,
+//       email: user.email,
+//       is_current_user: false, // This can be set based on your logic
+//     },
+//     role: "User", // Default role, you can modify this based on your logic
+//     added_since: new Date(user.date_joined).toLocaleDateString(),
+//     last_activity: new Date(user.last_login).toLocaleDateString(),
+//   }));
+// }
 
 export default function TeamPage() {
   const {
     props: { users },
   } = useValidatedProps<LoginChoiceProps>(LoginChoiceProps);
 
-  const data = getData(users as UserProps[]);
+  const tableColumns = useMemo(() => columns("ion"), []);
+
+  // const data = getData(users as UserProps[]);
 
   console.log("We have users data:", users);
 
@@ -48,7 +51,7 @@ export default function TeamPage() {
         </div>
       </div>
 
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={tableColumns} data={users as User[]} />
     </div>
   );
 }
