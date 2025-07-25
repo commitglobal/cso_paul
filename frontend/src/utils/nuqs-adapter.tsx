898645a2fdea9530409abf5162f5b97c39d65f9a
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-07-25 16:20:01 +0300

    feat: Refactor nuqs adapter by separating logic into a new file

--- frontend/src/utils/nuqs-adapter.tsx
+++ frontend/src/utils/nuqs-adapter.tsx
@@ -1,22 +1,4 @@
-// Adapter using custom provider from nuqs:
-import { router, usePage } from "@inertiajs/react";
-import { unstable_createAdapterProvider, renderQueryString } from "nuqs/adapters/custom";
-
-export function useNuqsInertiaAdapter() {
-  const { url } = usePage();
-  const searchParams = new URL(location.origin + url).searchParams;
-
-  function updateUrl(search, options) {
-    const newUrl = new URL(location.href);
-    newUrl.search = renderQueryString(search);
-    router.visit(newUrl.pathname + newUrl.search, {
-      replace: options.history === "replace",
-      preserveScroll: !options.scroll,
-      preserveState: options.shallow,
-    });
-  }
-
-  return { searchParams, updateUrl };
-}
+import { unstable_createAdapterProvider } from "nuqs/adapters/custom";
+import { useNuqsInertiaAdapter } from "./nuqs-inertia-adapter";
 
 export const NuqsInertiaAdapter = unstable_createAdapterProvider(useNuqsInertiaAdapter);
